ae105daa41713aa6a2ca69c9dec08831b1b18788
Ivan Miano <ivanmiano1718@gmail.com>
2025-02-16 15:39:25 +0800

    Cleaned up the code a bit. ALso removed all comments for implementations of the BST. Only added comments on new implementations for the AVL variant, such as rotations, balance factor, and height.

--- AVLTree.java
+++ AVLTree.java
@@ -4,7 +4,7 @@ import java.util.Queue;
 public class AVLTree<T extends Comparable<T>> {
     Node root;
 
-    // INSERTION
+    // INSERTION (UPDATED)
     public void insert(T data) {
         root = insert(root, data);
     }
@@ -13,46 +13,109 @@ public class AVLTree<T extends Comparable<T>> {
         if (root == null) {
             return new Node(data);
         }
-
+        
         if (data.compareTo(root.data) < 0) {
             root.left = insert(root.left, data);
         } else if (data.compareTo(root.data) > 0) {
             root.right = insert(root.right, data);
+        } else {
+            // case for duplicates
         }
-        updateHeight(root);
-        return rotate(root);
+        // return root;
+        updateHeight(root); // updates the height of each visited node
+        return rotate(root); // returns the rotated subtree (if there's any rotations)
     }
 
-    // DELETION
-    public void delete(T data) { 
+    // DELETION (UPDATED)
+    public void delete(T data) {
         root = delete(root, data);
     }
 
     private Node delete(Node root, T data) {
+        if (root == null) {
+            return null;
+        }
+
         if (data.compareTo(root.data) < 0) {
             root.left = delete(root.left, data);
         } else if (data.compareTo(root.data) > 0) {
             root.right = delete(root.right, data);
-        } else { // found the data to be deleted
-            if (root.left == null) { // case: right child only or leaf node
-                return root.right;
-            } else if (root.right == null) { // case: left child only
+        } else {
+            if (root.right == null) {
                 return root.left;
-            } else { // case: two child
-                root.data = getMin(root.right); // replace with the successor
-                root.right = delete(root.right, root.data); // delete the successor (which is now a duplicate) from the right subtree
+            } else if (root.left == null) {
+                return root.right;
+            } else {
+                root.data = getMax(root.left);
+                root.left = delete(root.left, root.data);
             }
         }
-        updateHeight(root);
-        return rotate(root);
+        // return root;
+        updateHeight(root); // updates the height of each visited node
+        return rotate(root); // return the rotated subtree (if there's any rotations)
     }
 
-    // SEARCHING (iterative)
+    // HEIGHT UPDATE
+    public void updateHeight(Node node) {
+        int leftHeight = (node.left != null ? node.left.height : -1);
+        int rightHeight = (node.right != null ? node.right.height : -1);
+        node.height = Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    // BALANCE FACTOR
+    private int getBalanceFactor(Node node) {
+        int leftHeight = (node.left != null ? node.left.height : -1);
+        int rightHeight = (node.right != null ? node.right.height : -1);
+        return leftHeight - rightHeight;
+    }
+
+    // ROTATIONS
+    public Node rotate(Node node) {
+        int balance = getBalanceFactor(node); // valid: [-1, 0, 1]
+        if (balance > 1) { // left heavy
+            if (getBalanceFactor(node.left) < 0) { // left-right
+                node.left = rotateLeft(node.left);
+            }
+            return rotateRight(node);
+        }
+        if (balance < -1) { // right heavy
+            if (getBalanceFactor(node.right) > 0) { // right-left
+                node.right = rotateRight(node.right);
+            }
+            return rotateLeft(node);
+        }
+        return node; // balanced, no rotations needed
+    }
+
+    // rotate left (when right heavy)
+    private Node rotateLeft(Node root) {
+        Node pivot = root.right;
+        root.right = pivot.left;
+        pivot.left = root;
+        updateHeight(root);
+        updateHeight(pivot);
+        return pivot;
+    }
+
+    // rotate right (when left heavy)
+    private Node rotateRight(Node root) {
+        Node pivot = root.left;
+        root.left = pivot.right;
+        pivot.right = root;
+        updateHeight(root);
+        updateHeight(pivot);
+        return pivot;
+    }
+
     public boolean contains(T data) {
+        if (isEmpty()) {
+            return false;
+        }
+
         Node curr = root;
         while (curr != null) {
             if (data.compareTo(curr.data) == 0) {
-                return true; // data found
+                return true;
             } else if (data.compareTo(curr.data) < 0) {
                 curr = curr.left;
             } else {
@@ -62,65 +125,153 @@ public class AVLTree<T extends Comparable<T>> {
         return false;
     }
 
-    // updates the height (longest path to a leaf node) of a given node
-    private void updateHeight(Node node) {
-        int maxHeight = Math.max((node.left != null ? node.left.height : -1),
-                                  (node.right != null ? node.right.height : -1));
-        node.height = maxHeight + 1;
+    public String inorder() {
+        if (isEmpty()) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        inorder(root, sb);
+        return "[" + sb.substring(0, sb.length() - 2) + "]";
     }
 
-    // gets the balance factor of a given node
-    private int getBalanceFactor(Node node) {
-        int leftHeight = (node.left != null ? node.left.height : -1);
-        int rightHeight = (node.right != null ? node.right.height : -1);
-        return leftHeight - rightHeight;
-    }
+    private StringBuilder inorder(Node root, StringBuilder sb) {
+        if (root == null) {
+            return sb;
+        }
         
-    // ROTATION
-    private Node rotate(Node node) {
-        int balance = getBalanceFactor(node);
-        if (balance > 1) { // left heavy
-            if (getBalanceFactor(node.left) < 0) { // left-right case
-                node.left = rotateLeft(node.left);
-            }
-            return rotateRight(node);
-        }
-        if (balance < -1) { // right heavy
-            if (getBalanceFactor(node.right) > 0) { // right-left case
-                node.right = rotateRight(node.right);
-            }
-            return rotateLeft(node);
-        }
-        return node;
+        inorder(root.left, sb);
+        sb.append(root.data).append(", ");
+        inorder(root.right, sb);
+        return sb;
     }
 
-    // rotate left (right heavy)
-    private Node rotateLeft(Node root) {
-        // System.out.println("Rotating left " + root.data);
-        Node pivot = root.right;
-        root.right = pivot.left;
-        pivot.left = root;
-        updateHeight(root);
-        updateHeight(pivot);
-        return pivot; 
+    public String levelorder() {
+        if (isEmpty()) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        levelorder(root, sb);
+        return "[" + sb.substring(0, sb.length() - 2) + "]";
+    }
+
+    private StringBuilder levelorder(Node root, StringBuilder sb) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+            if (curr.left != null) {
+                q.add(curr.left);
+            }
+            if (curr.right != null) {
+                q.add(curr.right);
+            }
+            sb.append(curr.data).append(", ");
+        }
+        return sb;
+    }
+
+    public T getMin() {
+        return getMin(root);
     }
     
-    // rotate right (left heavy)
-    private Node rotateRight(Node root) {
-        // System.out.println("Rotating right " + root.data);
-        Node pivot = root.left;
-        root.left = pivot.right;
-        pivot.right = root;
-        updateHeight(root);
-        updateHeight(pivot);
-        return pivot;
+    private T getMin(Node root) {
+        if (root == null) {
+            return null;
+        }
+        
+        Node curr = root;
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr.data;
+    }
+    
+    public T getMax() {
+        return getMax(root);
+    }
+    
+    private T getMax(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Node curr = root;
+        while (curr.right != null) {
+            curr = curr.right;
+        }
+        return curr.data;
     }
 
+    public boolean isEmpty() {
+        return root == null;
+    }
+    
 
-    // FOR TESTING ONLY
 
-    // testing hehe (for checking if tree structure is correct)
-    public void getChildren(T data) {
+    // =========================================================
+    // ================ TESTING (FOR FUN ONLY!) ================  
+    // =========================================================
+
+    // return the root of the tree
+    public T getRoot() {
+        return (root != null ? root.data : null);
+    }
+
+    // find the depth of a given node
+    public int getDepth(T data) {
+        Node curr = root;
+        int depth = 0;
+        while (curr != null) {
+            if (data.compareTo(curr.data) == 0) {
+                return depth;
+            } else if (data.compareTo(curr.data) < 0) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+            depth++;
+        }
+        return -1;
+    }
+
+    // find the height of the tree
+    public int getHeight() {
+        return getHeight(root);
+    }
+    
+    // find the height of a given node
+    public int getHeight(T data) {
+        Node curr = root;
+        while (curr != null) {
+            if (data.compareTo(curr.data) == 0) {
+                return getHeight(curr);
+            } else if (data.compareTo(curr.data) < 0) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+        return -1; // tree is empty or data is not in the tree
+    }
+
+    private int getHeight(Node root) {
+        // base case
+        if (root == null) {
+            return -1;
+        }
+
+        // recursive case
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    
+    // display the children of a given node
+    public void printChildren(T data) {
         Node curr = root;
         while (curr != null) {
             if (data.compareTo(curr.data) == 0) {
@@ -136,145 +287,16 @@ public class AVLTree<T extends Comparable<T>> {
         System.out.println("Data is nonexistent.");
     }
 
-    // testing for height checker
-    public void getHeight(T data) {
-        Node curr = root;
-        while (curr != null) {
-            if (data.compareTo(curr.data) == 0) {
-                System.out.println(data + " height: " + curr.height);
-                return;
-            } else if (data.compareTo(curr.data) < 0) {
-                curr = curr.left;
-            } else {
-                curr = curr.right;
-            }
-        }
-        System.out.println("Data is nonexistent.");
-    }
+    // =========================================================
+    // =========================================================
+    // =========================================================
     
-    // testing for depth checker
-    public void getDepth(T data) {
-        Node curr = root;
-        int depth = 0;
-        while (curr != null) {
-            if (data.compareTo(curr.data) == 0) {
-                System.out.println(data + " depth: " + depth);
-                return;
-            } else if (data.compareTo(curr.data) < 0) {
-                curr = curr.left;
-            } else {
-                curr = curr.right;
-            }
-            depth++;
-        }
-        System.out.println("Data is nonexistent.");
-    }
 
 
-    // TRAVERSAL
-    public void inorder() {
-        inorder(root);
-        System.out.println();
-    }
-
-    private void inorder(Node root) {
-        if (root == null) {
-            return;
-        }
-
-        inorder(root.left);
-        System.out.print(root.data + " ");
-        inorder(root.right);
-    }
-
-    public void preorder() {
-        preorder(root);
-        System.out.println();
-    }
-
-    private void preorder(Node root) {
-        if (root == null) {
-            return;
-        }
-
-        System.out.print(root.data + " ");
-        preorder(root.left);
-        preorder(root.right);
-    }
-
-    public void postorder() {
-        postorder(root);
-        System.out.println();
-    }
-
-    private void postorder(Node root) {
-        if (root == null) {
-            return;
-        }
-
-        postorder(root.left);
-        postorder(root.right);
-        System.out.print(root.data + " ");
-    }
-
-    public void levelorder() {
-        if (root == null) {
-            return;
-        }
-
-        levelorder(root);
-    }
-
-    private void levelorder(Node root) {
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            Node curr = q.poll();
-            if (curr.left != null) {
-                q.add(curr.left);
-            }
-            if (curr.right != null) {
-                q.add(curr.right);
-            }
-            System.out.print(curr.data + " ");
-        }
-        System.out.println();
-    }
-
-    // get the maximum value of a given node subtree
-    public T getMax(Node root) {
-        if (root == null) {
-            return null;
-        }
-
-        Node curr = root;
-        while (curr.right != null) {
-            curr = curr.right;
-        }
-        return curr.data;
-    }
-
-    // get the minimum value of a given node subtree
-    public T getMin(Node root) {
-        if (root == null) {
-            return null;
-        }
-
-        Node curr = root;
-        while (curr.left != null) {
-            curr = curr.left;
-        }
-        return curr.data;
-    }
-
-    public boolean isEmpty() {
-        return root == null;
-    }
-
-    // Node class structure for this AVL Tree implementation
+    // TREE NODE CLASS STRUCTURE FOR AVL TREE IMPLEMENTATION
     class Node {
         T data;
-        int height;
+        int height; // for checking the balance factor
         Node left;
         Node right;
 
