19feab96a17de6f71fdc03985119c916de70ac69
Ivan Miano <ivanmiano1718@gmail.com>
2025-02-16 15:01:45 +0800

    Cleaned up the code a bit

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -4,158 +4,70 @@ import java.util.Queue;
 public class BinarySearchTree<T extends Comparable<T>> {
     Node root;
 
-    // testing for finding the height of a tree (through finding the height of subtrees recursively)
-    public int getHeight() {
-        return getHeight(root);
+    // INSERTION
+    public void insert(T data) {
+        root = insert(root, data);
     }
 
-    private int getHeight(Node root) {
-        // base case
+    private Node insert(Node root, T data) {
         if (root == null) {
-            return 0;
-        }
-
-        // recursive calls
-        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
-    }
-
-    // insertion 1 (geek)
-    public void add(T value) {
-        if (root == null) {
-            root = new Node(value);
-        } else {
-            add(root, value);
-        }
-    }
-
-    private void add(Node root, T value) {
-        if (value.compareTo(root.value) < 0) {
-            // left child
-            if (root.left == null) {
-                root.left = new Node(value);
-            } else {
-                add(root.left, value);
-            }
-        } else if (value.compareTo(root.value) > 0) {
-            // right child
-            if (root.right == null) {
-                root.right = new Node(value);
-            } else {
-                add(root.right, value);
-            }
-        } else {
-            // case for duplicates
-        }
-    }
-
-    // insertion 2 (bro)
-    public void insert(T value) {
-        root = insert(root, value);
-    }
-
-    private Node insert(Node root, T value) {
-        if (root == null) {
-            return new Node(value);
+            return new Node(data);
         }
         
-        if (value.compareTo(root.value) < 0) {
-            root.left = insert(root.left, value);
-        } else if (value.compareTo(root.value) > 0) {
-            root.right = insert(root.right, value);
+        if (data.compareTo(root.data) < 0) {
+            // go left child
+            root.left = insert(root.left, data);
+        } else if (data.compareTo(root.data) > 0) {
+            // go right child
+            root.right = insert(root.right, data);
         } else {
             // case for duplicates
         }
         return root;
     }
 
-    // deletion 1 (geek)
-    /*
-     * the logic here is checking if the right child is null, if it's null, it solves the problem for left child only or no child
-     * since in both cases, right child is null. then if right is not null, we check if left is null, if it is, solve the problem
-     * for right child only. the else{} is for when the root has two children, where we can pick either the successor or predecessor 
-     */
-    public void delete(T value) {
-        root = delete(root, value);
+    // DELETION
+    public void delete(T data) {
+        root = delete(root, data);
     }
 
-    public Node delete(Node root, T value) {
+    private Node delete(Node root, T data) {
         if (root == null) {
-            return root;
+            return null; // to avoid NullPointerException when the data is not in the tree
         }
 
-        if (value.compareTo(root.value) == 0) {
+        if (data.compareTo(root.data) < 0) {
+            // go left child
+            root.left = delete(root.left, data);
+        } else if (data.compareTo(root.data) > 0) {
+            // go right child
+            root.right = delete(root.right, data);
+        } else {
+            // data to be deleted is found
             if (root.right == null) {
-                // left child only or no children/leaf node
-                return root.left;
+                return root.left; // left child only or leaf node
             } else if (root.left == null) {
-                // right child only
-                return root.right;
+                return root.right; // right child only
             } else {
                 // two children (either get predecessor or successor, both are valid. in this case it's predecessor)
-                root.value = getPredecessor(root);
-                root.left = delete(root.left, root.value);
+                root.data = getMax(root.left);
+                root.left = delete(root.left, root.data); // delete the predecessor (which is now a duplicate)
             }
-        } else if (value.compareTo(root.value) < 0) {
-            // go left child
-            root.left = delete(root.left, value);
-        } else {
-            // go right child
-            root.right = delete(root.right, value);
         }
         return root;
     }
 
-    // deletion 2 (bro)
-    /*
-     * the difference in here is that the logic always goes for the successor/predecessor if it's not a leaf node,
-     * unlike the first one that replaces the to-be-deleted node with its one child, handling the two child case
-     * separately
-     */
-    public void remove(T value) {
-        root = remove(root, value);
-    }
-
-    private Node remove(Node root, T value) {
-        if (root == null) {
-            return root;
-        }
-
-        if (value.compareTo(root.value) == 0) {
-            if (root.left == null && root.right == null) {
-                // no children/leaf node
-                return null;
-            } else if (root.left != null) {
-                // replace root's value with predecessor (max value in left subtree)
-                root.value = getPredecessor(root);
-                // then remove the predecessor in the left subtree
-                root.left = remove(root.left, root.value);
-            } else {
-                // replace root's value with successor (min value in right subtree)
-                root.value = getSuccessor(root);
-                // then remove the successor in the right subtree
-                root.right = remove(root.right, root.value);
-            }
-        } else if (value.compareTo(root.value) < 0) {
-            // go left child
-            root.left = remove(root.left, value);
-        } else {
-            // go right child
-            root.right = remove(root.right, value);
-        }
-        return root;
-    }
-
-    // Search Iterative
-    public boolean contains(T value) {
+    // SEARCHING (ITERATIVE)
+    public boolean contains(T data) {
         if (isEmpty()) {
             return false;
         }
 
         Node curr = root;
         while (curr != null) {
-            if (value.compareTo(curr.value) == 0) {
+            if (data.compareTo(curr.data) == 0) {
                 return true;
-            } else if (value.compareTo(curr.value) < 0) {
+            } else if (data.compareTo(curr.data) < 0) {
                 curr = curr.left;
             } else {
                 curr = curr.right;
@@ -164,54 +76,40 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return false;
     }
 
-    // Search Recursive
-    public boolean search(T value) {
-        return search(root, value);
-    }
-
-    private boolean search(Node root, T value) {
-        // Tree is either empty or does not contain the value (reached the end)
-        if (root == null) {
-            return false;
+    // TRAVERSAL (INORDER)
+    public String inorder() {
+        if (isEmpty()) {
+            return "[]";
         }
 
-        if (value.compareTo(root.value) == 0) {
-            return true;
-        } else if (value.compareTo(root.value) < 0) {
-            return search(root.left, value);
-        } else {
-            return search(root.right, value);
-        }
+        StringBuilder sb = new StringBuilder();
+        inorder(root, sb);
+        return "[" + sb.substring(0, sb.length() - 2) + "]";
     }
 
-    // traversal
-    public void inorder() {
+    private StringBuilder inorder(Node root, StringBuilder sb) {
         if (root == null) {
-            return;
-        }
-
-        inorder(root);
-    }
-
-    private void inorder(Node root) {
-        if (root == null) {
-            return;
+            return sb;
         }
         
-        inorder(root.left);
-        System.out.println(root.value);
-        inorder(root.right);
+        inorder(root.left, sb);
+        sb.append(root.data).append(", ");
+        inorder(root.right, sb);
+        return sb;
     }
 
-    public void levelorder() {
-        if (root == null) {
-            return;
+    // TRAVERSAL (LEVEL-ORDER)
+    public String levelorder() {
+        if (isEmpty()) {
+            return "[]";
         }
 
-        levelorder(root);
+        StringBuilder sb = new StringBuilder();
+        levelorder(root, sb);
+        return "[" + sb.substring(0, sb.length() - 2) + "]";
     }
 
-    private void levelorder(Node root) {
+    private StringBuilder levelorder(Node root, StringBuilder sb) {
         Queue<Node> q = new LinkedList<>();
         q.add(root);
 
@@ -223,39 +121,144 @@ public class BinarySearchTree<T extends Comparable<T>> {
             if (curr.right != null) {
                 q.add(curr.right);
             }
-            System.out.println(curr.value);
+            sb.append(curr.data).append(", ");
         }
+        return sb;
     }
 
-    // successor
-    private T getSuccessor(Node root) {
-        root = root.right;
-        while (root.left != null) {
-            root = root.left;
+    // get min element in the tree
+    public T getMin() {
+        return getMin(root);
+    }
+    
+    // GET MINIMUM (helper method for getting successor)
+    private T getMin(Node root) {
+        if (root == null) {
+            return null;
         }
-        return root.value;
+        
+        Node curr = root;
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr.data;
+    }
+    
+    // get max element in the tree
+    public T getMax() {
+        return getMax(root);
+    }
+    
+    // GET MAXIMUM (helper method for getting predecessor)
+    private T getMax(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Node curr = root;
+        while (curr.right != null) {
+            curr = curr.right;
+        }
+        return curr.data;
     }
 
-    // predecessor
-    private T getPredecessor(Node root) {
-        root = root.left;
-        while (root.right != null) {
-            root = root.right;
-        }
-        return root.value;
-    }
-
+    // IS EMPTY
     public boolean isEmpty() {
         return root == null;
     }
+    
 
+
+    // =========================================================
+    // ================ TESTING (FOR FUN ONLY!) ================  
+    // =========================================================
+
+    // return the root of the tree
+    public T getRoot() {
+        return (root != null ? root.data : null);
+    }
+
+    // find the depth of a given node
+    public int getDepth(T data) {
+        Node curr = root;
+        int depth = 0;
+        while (curr != null) {
+            if (data.compareTo(curr.data) == 0) {
+                return depth;
+            } else if (data.compareTo(curr.data) < 0) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+            depth++;
+        }
+        return -1;
+    }
+
+    // find the height of the tree
+    public int getHeight() {
+        return getHeight(root);
+    }
+    
+    // find the height of a given node
+    public int getHeight(T data) {
+        Node curr = root;
+        while (curr != null) {
+            if (data.compareTo(curr.data) == 0) {
+                return getHeight(curr);
+            } else if (data.compareTo(curr.data) < 0) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+        return -1; // tree is empty or data is not in the tree
+    }
+
+    private int getHeight(Node root) {
+        // base case
+        if (root == null) {
+            return -1;
+        }
+
+        // recursive case
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    
+    // display the children of a given node
+    public void printChildren(T data) {
+        Node curr = root;
+        while (curr != null) {
+            if (data.compareTo(curr.data) == 0) {
+                System.out.println("Left: " + (curr.left != null ? curr.left.data : null));
+                System.out.println("Right: " + (curr.right != null ? curr.right.data : null));
+                return;
+            } else if (data.compareTo(curr.data) < 0) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+        System.out.println("Data is nonexistent.");
+    }
+
+    // =========================================================
+    // =========================================================
+    // =========================================================
+    
+
+
+    // TREE NODE CLASS STRUCTURE
     class Node {
-        T value;
+        T data;
         Node left;
         Node right;
 
-        Node(T value) {
-            this.value = value;
+        Node(T data) {
+            this.data = data;
         }
     }
 }
